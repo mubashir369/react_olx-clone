dbee92cc8f9e927bdb969d74895a7ce52d0126d3
mubashir369 <muhammedmubashir3693@gmail.com>
2022-06-29 19:31:19 +0530

    Finish Login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,14 +1,26 @@
-import React from 'react';
-
+import React,{useState,useContext} from 'react';
+import {FirebaseContext} from '../../store/FirebaseContext'
 import Logo from '../../olx-logo.png';
 import './Login.css';
-
+import {useHistory} from 'react-router-dom'
 function Login() {
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
+const {firebase}=useContext(FirebaseContext)
+const history=useHistory()
+  const handleLogin=(e)=>{
+    e.preventDefault()
+    firebase.auth().signInWithEmailAndPassword(email,password).then(()=>{
+      history.push('/')
+    }).catch((error)=>{
+      alert(error)
+    })
+  }
   return (
     <div>
       <div className="loginParentDiv">
         <img width="200px" height="200px" src={Logo}></img>
-        <form>
+        <form onSubmit={handleLogin}>
           <label htmlFor="fname">Email</label>
           <br />
           <input
@@ -17,6 +29,8 @@ function Login() {
             id="fname"
             name="email"
             defaultValue="John"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
           />
           <br />
           <label htmlFor="lname">Password</label>
@@ -27,6 +41,8 @@ function Login() {
             id="lname"
             name="password"
             defaultValue="Doe"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
           />
           <br />
           <br />
